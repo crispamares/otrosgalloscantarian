8e5963d255583fcfedebab36f04a273fa45f4b8e
Jorge Peña Pastor <jorge.pena.pastor@gmail.com>
2011-05-07 16:03:19 +0200

    Votos por Provincia

--- src/checkDataStore.py
+++ src/checkDataStore.py
@@ -11,11 +11,18 @@ class MainPage(webapp.RequestHandler):
     #self.response.headers['Content-Type'] = 'text/plain'
     self.response.out.write('Hello, Gallo!!<br />')
     censos = model.CensoElectoral.all()
-    #censos.filer('municipio=','Adra')
+    censos.filter("comunidad =","Castilla - La Mancha")
     for censo in censos:
-        self.response.out.write(censo.comunidad + ":")
-        self.response.out.write(censo.provincia + ":")
-        self.response.out.write(censo.municipio + "<br />")
+        self.response.out.write("<b>" + censo.comunidad + ":")
+        self.response.out.write(censo.provincia + " Num. Habitantes: ")
+        self.response.out.write(censo.poblacion)
+        self.response.out.write("</b> <br />")
+        escrutinios = model.Escrutinio.all()
+        escrutinios.filter("censo  =",censo)
+        for escrutinio in escrutinios:
+            self.response.out.write("    " + escrutinio.partido + "=>")
+            self.response.out.write(escrutinio.votos)
+            self.response.out.write("<br />")
 
 application = webapp.WSGIApplication(
                                      [('/', MainPage)],
--- src/escrutor.py
+++ src/escrutor.py
@@ -15,6 +15,10 @@ VOTOSCANDIDATURA = 10
 VOTOSBLANCO = 11
 VOTOSNULO = 12
 
+class Provincia:
+    def __init__(self):
+        self.poblacion = 0
+        self.censoTotal = 0
 
 def LoadFile(filepath):
     voteReader = csv.reader(open(filepath,'rb'),delimiter=",")
@@ -26,31 +30,73 @@ def LoadFile(filepath):
     nombres = voteReader.next()
     siglas = voteReader.next()
     partidos = {}
+
+    votos = {}
+
     for sigla in siglas[13:]:
         id = siglas.index(sigla)
         partidos[id] = (nombres[id],sigla)
 
     for row in voteReader:
-        censo = CensoElectoral()
-        i = filepath.find('/')
-        censo.ano = filepath[i+1:i+5]
-        censo.comunidad = row[COMUNIDAD].strip()
-        censo.provincia = row[PROVINCIA].strip()
-        censo.municipio = row[MUNICIPIO].strip()
-        censo.censoTotal = row[CENSOTOTAL].strip()
-        censo.votosTotales = row[VOTOSTOTALES].strip()
-        censo.votosValidos = row[VOTOSVALIDOS].strip()
-        censo.votosCandidatura = row[VOTOSCANDIDATURA].strip()
-        censo.votosBlanco = row[VOTOSBLANCO].strip()
-        censo.votosNulo = row[VOTOSNULO].strip()
-        censo.put()
+        # Actualizar Informacion Provincia
+        comunidad = row[COMUNIDAD].strip()
+        provincia = row[PROVINCIA].strip()
+        municipio = row[MUNICIPIO].strip()
+        poblacionMunicipio = eval(row[POBLACION].strip().replace(",",""))
+        censoMunicipio = eval(row[CENSOTOTAL].strip().replace(",",""))
+        totalVotosMunicipio = eval(row[VOTOSTOTALES].strip().replace(",",""))
+        validosMunicipio = eval(row[VOTOSVALIDOS].strip().replace(",",""))
+        candidaturasMunicipio = eval(row[VOTOSCANDIDATURA].strip().replace(",",""))
+        blancoMunicipio = eval(row[VOTOSBLANCO].strip().replace(",",""))
+        nulosMunicipio = eval(row[VOTOSNULO].strip().replace(",",""))
+        if (not comunidad in votos):
+            votos[comunidad] = {}
+        if (not provincia in votos[comunidad]):
+            votos[comunidad][provincia] = {'Poblacion':0,'Censo':0,'TotalVotos':0,'Validos':0,'Candidaturas':0,'Blanco':0,'Nulos':0,'Votos':{}}
+        votos[comunidad][provincia]['Poblacion'] += poblacionMunicipio
+        votos[comunidad][provincia]['Censo'] += censoMunicipio
+        votos[comunidad][provincia]['TotalVotos'] += totalVotosMunicipio
+        votos[comunidad][provincia]['Validos'] += validosMunicipio
+        votos[comunidad][provincia]['Candidaturas'] += candidaturasMunicipio
+        votos[comunidad][provincia]['Blanco'] += blancoMunicipio
+        votos[comunidad][provincia]['Nulos'] += nulosMunicipio
 
+        # Actualizar votos partidos
         for idPartido in range(13,len(row)):
-            escrutinio = Escrutinio()
-            escrutinio.censo = censo
-            escrutinio.partido = partidos[idPartido][0]
-            escrutinio.votos = row[idPartido]
-            escrutinio.put()
+            sigla = siglas[idPartido]
+            votosMunicipio = eval(row[idPartido].replace(",",""))
+            if (votosMunicipio > 0):
+                if (not sigla in votos[comunidad][provincia]['Votos']):
+                    votos[comunidad][provincia]['Votos'][sigla] = votosMunicipio
+                else:
+                    votos[comunidad][provincia]['Votos'][sigla] += votosMunicipio
+
+    # El ano es el mismo para todos
+    i = filepath.find('/')
+    ano = filepath[i+1:i+5]
+
+    WriteToDataStore(ano, votos)
 
 
+def WriteToDataStore(ano, votos):
+    for comunidad in votos:
+        for provincia in votos[comunidad]:
+            censo = CensoElectoral()
+            censo.ano = ano
+            censo.comunidad = comunidad
+            censo.provincia = provincia
+            censo.poblacion = votos[comunidad][provincia]['Poblacion']
+            censo.censoTotal = votos[comunidad][provincia]['Censo']
+            censo.votosTotales = votos[comunidad][provincia]['TotalVotos']
+            censo.votosValidos = votos[comunidad][provincia]['Validos']
+            censo.votosCandidatura = votos[comunidad][provincia]['Candidaturas']
+            censo.votosBlanco = votos[comunidad][provincia]['Blanco']
+            censo.votosNulo = votos[comunidad][provincia]['Nulos']
+            censo.put()
 
+            for partido in votos[comunidad][provincia]['Votos']:
+                escrutinio = Escrutinio()
+                escrutinio.censo = censo
+                escrutinio.partido = partido
+                escrutinio.votos = votos[comunidad][provincia]['Votos'][partido]
+                escrutinio.put()
